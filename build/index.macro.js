a4d65ecdff644f18f31ac55749d724fcf139a0d4
Serkan Bircan <serkanbircan21@yandex.com>
2021-07-18 12:09:39 +0300

    Update builds

--- build/index.macro.js
+++ build/index.macro.js
@@ -1,6 +1,11 @@
-import { createMacro } from "babel-plugin-macros";
-import useStore from "./useStoreMacro";
-var useStoreMacro = createMacro(useStore, {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var babel_plugin_macros_1 = require("babel-plugin-macros");
+var useStoreMacro_1 = __importDefault(require("./useStoreMacro"));
+var useStoreMacro = babel_plugin_macros_1.createMacro(useStoreMacro_1.default, {
     configName: "zustandMacro"
 });
-export default useStoreMacro;
+exports.default = useStoreMacro;
--- build/useStoreMacro.js
+++ build/useStoreMacro.js
@@ -1,27 +1,33 @@
-import { addNamed as addImportDeclaration } from "@babel/helper-module-imports";
-import { MacroError } from "babel-plugin-macros";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var helper_module_imports_1 = require("@babel/helper-module-imports");
+var babel_plugin_macros_1 = require("babel-plugin-macros");
 var useStoreMacro = function (_a) {
     var references = _a.references, state = _a.state, babel = _a.babel, config = _a.config;
     var defaultImport = references.default || [];
     var t = babel.types;
     if (!config)
-        throw new MacroError("Macro config must be provided in package.json");
+        throw new babel_plugin_macros_1.MacroError("Macro config must be provided in package.json");
     var macroConfig = config.useStore;
     defaultImport.forEach(function (referencePath) {
         var _a;
         var functionCallPath = referencePath.parentPath;
         if ((functionCallPath === null || functionCallPath === void 0 ? void 0 : functionCallPath.type) !== "CallExpression")
-            throw new MacroError("useStoreMacro only supports function calls.");
-        addImportDeclaration(referencePath, macroConfig.importName, macroConfig.importSource, {
+            throw new babel_plugin_macros_1.MacroError("useStoreMacro only supports function calls.");
+        helper_module_imports_1.addNamed(referencePath, macroConfig.importName, macroConfig.importSource, {
             nameHint: "_" + macroConfig.importName
         });
         var idPath = (_a = functionCallPath.parentPath) === null || _a === void 0 ? void 0 : _a.get("id");
         if (!t.isObjectPattern(idPath))
-            throw new MacroError("useStoreMacro only supports object destructuring.");
-        var deconstructedProps = idPath.node.properties.map(function (x) { return x.value.name; });
-        var useStoreParameters = deconstructedProps === null || deconstructedProps === void 0 ? void 0 : deconstructedProps.map(function (x) { return x + ": s." + x; }).join(",");
+            throw new babel_plugin_macros_1.MacroError("useStoreMacro only supports object destructuring.");
+        var deconstructedProps = idPath.node
+            .properties
+            .map(function (x) { return x.value.name; });
+        var useStoreParameters = deconstructedProps
+            .map(function (x) { return x + ": s." + x; })
+            .join(",");
         var useShallowStoreNode = babel.template.expression.ast("\n      _" + macroConfig.importName + "((s) => ({\n        " + useStoreParameters + "\n      }))\n    ");
         functionCallPath === null || functionCallPath === void 0 ? void 0 : functionCallPath.replaceWith(useShallowStoreNode);
     });
 };
-export default useStoreMacro;
+exports.default = useStoreMacro;
